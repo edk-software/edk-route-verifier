7e3f371a9e550ec4dd3617369a3dbcaeb6519cd2
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2019-12-02 10:54:29 +0100

    ERV-97 - Add error handling and improve API server security (#111)
    * Added error handling in server
    * Improved API server security (added helmet library + introduced basic authentication)
    * Improved CircleCI configuration to setup API User/Pass

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
+import basicAuth from 'express-basic-auth';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import helmet from 'helmet';
 import express from 'express';
 import logger from 'loglevel';
 
@@ -7,9 +9,38 @@ import verifyRoute from '../core/verifyRoute.js';
 import RouteVerificationInput from '../data/RouteVerificationInput.js';
 import RouteVerificationOptions from '../data/RouteVerificationOptions.js';
 
+function secureServer(app, config) {
+    app.use(helmet());
+
+    const { apiUser, apiPass } = config;
+    if (apiUser && apiPass) {
+        app.use(
+            basicAuth({
+                users: { [apiUser]: apiPass }
+            })
+        );
+    } else {
+        logger.warn(
+            'Server API username/password not defined in the configuration. ' +
+                'Starting server without Basic Authentication.'
+        );
+    }
+}
+
+function setupLogger(debug) {
+    if (debug) {
+        logger.setLevel('debug');
+    } else {
+        logger.setLevel('info');
+    }
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export function startServer(config, port = 9102, language = 'en', debug = false) {
     const app = express();
+
+    secureServer(app, config);
+    setupLogger(debug);
 
     app.post('/api/verify', cors(), bodyParser.json(), (req, res) => {
         const { kml, language: l = language, debug: d = debug } = req.body;
@@ -18,23 +49,16 @@ export function startServer(config, port = 9102, language = 'en', debug = false)
         const verificationOption = new RouteVerificationOptions(config, l, d);
 
         verifyRoute(routeData, verificationOption)
-            .then(verificationOutput => {
-                res.send(verificationOutput);
-            })
+            .then(verificationOutput => res.send(verificationOutput))
             .catch(error => {
-                // TODO handle errors
-                res.send({});
+                logger.error(error);
+
+                return res.status(500).send({
+                    message: 'Unexpected internal server error. Check logs.'
+                });
             });
     });
 
-    // TODO block other endpoints
-
-    if (debug) {
-        logger.setLevel('debug');
-    } else {
-        logger.setLevel('info');
-    }
-
-    logger.info(`Starting server at http://localhost:${port}.`);
+    logger.info(`Starting Verify API server at http://localhost:${port}.`);
     app.listen(port);
 }
