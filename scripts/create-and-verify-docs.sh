3c824e613c420cd9e35508df9eeecf3c9dbc0d3e
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2019-12-05 09:15:50 +0100

    Updated npm scripts

--- scripts/create-and-verify-docs.sh
+++ scripts/create-and-verify-docs.sh
@@ -2,13 +2,22 @@
 set -e
 
 DOC_PATH="docs/USAGE.md"
+
+# Create docs
+CLI_HELP=$(npm run --silent start:cli -- --help)
+echo "\`\`\`" > ${DOC_PATH}
+echo "${CLI_HELP}" >> ${DOC_PATH}
+echo "\`\`\`" >> ${DOC_PATH}
+
+# Verify docs
 git diff --exit-code ./${DOC_PATH}
 EXIT_CODE=$?
 
 if [ "$EXIT_CODE" != "0" ]; then
+  echo ""
   echo "ERROR: CLI help usage documentation not up-to-date with the source code. Follow these steps:"
-  echo " - run 'npm run docs:create',"
   echo " - check output '${DOC_PATH}' file,"
   echo " - commit updated '${DOC_PATH}' file."
+  echo ""
   exit 1
 fi;
